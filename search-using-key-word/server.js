5a637e699ce31dcc4f8463427ddc51a7b1d4ba7b
shovan04 <shovanm50@gmail.com>
2022-08-30 07:45:44 +0530

    update index.html | server.js | server.php

--- search-using-key-word/server.js
+++ search-using-key-word/server.js
@@ -1,1 +1,28 @@
-Ajax({url:"server.php",method:"POST",data:{first:!0},success(a){setHtml("tbl",a)},error(a){console.log(a)}}),btnclk("search",()=>{let a=getVal("searchKey");""==a?(setHtml("tbl",""),setTxt("err","Please enter Product Name")):(setHtml("err",""),Ajax({url:"server.php",method:"POST",data:{st:!0,search:a},success(a){setHtml("tbl",a)},error(a){console.log(a)}}))})
+Ajax({
+  url: "server.php",
+  method: "POST",
+  data: { first: !0 },
+  success(a) {
+    setHtml("tbl", a);
+  },
+  error(a) {
+    console.log(a);
+  },
+}),
+  btnclk("search", () => {
+    let a = getVal("searchKey");
+    "" == a
+      ? (setHtml("tbl", ""), setTxt("err", "Please enter Product Name"))
+      : (setHtml("err", ""),
+        Ajax({
+          url: "server.php",
+          method: "POST",
+          data: { st: !0, search: a },
+          success(a) {
+            setHtml("tbl", a);
+          },
+          error(a) {
+            console.log(a);
+          },
+        }));
+  });
