0e4ee0172c129558b483729ac400a33a70397519
shovan04 <shovanm50@gmail.com>
2022-08-30 08:59:25 +0530

    update server.php file

--- search-using-key-word/server.php
+++ search-using-key-word/server.php
@@ -1,8 +1,4 @@
 <?php
-require 'vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
 $conn = mysqli_connect("localhost","root","","test");
 if (!$conn) {
     die();
